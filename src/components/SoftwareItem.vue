1d608829660517ff96dfef322445e51f83eb5826
Aokashi <Aokashi.Towa@Gmail.com>
2019-06-25 00:02:00 +0900

    ソフトウェアページとデータを追加

---
+++ src/components/SoftwareItem.vue
@@ -0,0 +1,36 @@
+<docs>
+## ソフトウェアアイテム
+ソフトウェアページの各ソフトウェアを表すコンポーネントです。
+
+### `data` について
+- `id`: ID
+- `name`: 名前
+- `description`: 説明
+- `repository`: GitHub リポジトリへのURL
+  - 絶対URLのため、 GitHub でなくても、 Bitbucket のリポジトリでも対応できます
+- `keywords`: キーワード
+- `references`: 外部ページへのURL (複数指定)
+  - `name`: 名前
+  - `url`: URL (絶対URL)
+</docs>
+
+<template>
+  <div>
+    <h2>{{data.name}}</h2>
+    <p>{{data.description}}</p>
+    <span v-for="(reference, referenceKey) in data.references" :key="referenceKey">
+      <a :href=reference.url>{{reference.name}}</a>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
---
+++ src/pages/Software.vue
@@ -0,0 +1,34 @@
+<template>
+  <Layout>
+    <header class="header">
+      <h1>ソフトウェア</h1>
+    </header>
+    <p>制作したソフトウェアを公開しちえます。公開しているプログラムはすべて GitHub にリポジトリを残しています。以下のリンクから GitHub へアクセスできます。</p>
+    <a class="button" href="https://github.com/aokashi/">GitHub へアクセス</a>
+    <div v-for="(software, softwareKey) in SoftwareData" :key="softwareKey">
+      <SoftwareItem :data=software />
+    </div>
+  </Layout>
+</template>
+
+<script>
+import SoftwareData from '~/data/software.json';
+import SoftwareItem from '~/components/SoftwareItem.vue';
+
+export default {
+  data() {
+    return {
+      SoftwareData
+    }
+  },
+  components: {
+    SoftwareItem
+  }
+}
+</script>
+
+<style>
+.button {
+  @apply bg-blue-500 text-white m-1 p-1;
+}
+</style>
