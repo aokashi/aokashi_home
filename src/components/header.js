e5869112546d0cbcfc33e1cdac95b45b7bf5ddc2
Aokashi <Aokashi.Towa@Gmail.com>
2019-09-17 00:39:27 +0900

    TailwindのCSSが入るように調整
    FIXME: Tailwind の apply ディレクティブが正しく動作しない

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,14 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import AokashiHomeLogo from "../images/ah-logo_mini.png"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
+  <header className="main-header">
+    <div className="main-header__container">
+      <h1 className="main-header__title">
+        <Link to="/">
+          <img src={AokashiHomeLogo} alt={siteTitle} />
         </Link>
       </h1>
     </div>
