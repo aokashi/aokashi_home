c64a5ae382aba1a5b288378efe233a1c81a2394e
Aokashi <Aokashi.Towa@Gmail.com>
2019-09-25 00:31:39 +0900

    ソフトウェアページを作成

---
+++ src/pages/software.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import Layout from '../layouts/page-layout'
+
+import SoftwareData from '../data/software.json'
+
+const SoftwarePage = () => (
+  <Layout>
+    <article>
+      <header className="article-header header">
+        <h1 className="header__title">ソフトウェア</h1>
+      </header>
+      <div className="article-content content">
+        <p>制作したプログラムを公開しています。基本的にここに公開しているプログラムはすべてGitHubにリポジトリを残しています。以下のリンクからGitHubへアクセスできます。</p>
+        <a className="link-button" href="https://github.com/aokashi">GitHubへアクセス</a>
+        <div className="note note--info">
+          <p>各プログラムのライセンスはGitHubのLICENSEファイルをご参照願います。</p>
+        </div>
+      </div>
+    </article>
+    {softwareList}
+  </Layout>
+)
+
+const softwareList = (
+  <div className="software-list list">
+    {
+      SoftwareData.map((item) => (
+        <div className="list__item">
+          <h3>{item.name}</h3>
+          <p>{item.description}</p>
+          <div className="item__keywords keywords">
+            {
+              item.keywords.map((keyword) => (
+                <span className="keywords__item">{keyword}</span>
+              ))
+            }
+          </div>
+          <div className="item__nav nav">
+            {
+              item.repository &&
+                <a className="nav__item nav__item--github-repo" href={item.repository}>GitHub リポジトリ</a>
+            }
+            {
+              item.references.map((link) => (
+                <a className="nav__item" href={link.url}>{link.name}</a>
+              ))
+            }
+          </div>
+        </div>
+      ))
+    }
+  </div>
+)
+
+export default SoftwarePage
