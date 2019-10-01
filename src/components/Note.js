3e279a673895e994c3785a8fe719a83d9d2a1a3d
Aokashi <Aokashi.Towa@Gmail.com>
2019-10-01 14:44:54 +0900

    note コンポーネントを Note コンポーネントに改名

---
+++ src/components/Note.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import styles from './Note.module.sass'
+
+const Note = ({ name, className, icon, children }) => (
+  <div className={`${styles.note} ${className}`}>
+    {
+      icon &&
+        <img className={styles.noteIcon} src={icon} alt={name}></img>
+    }
+    {children}
+  </div>
+)
+
+Note.propTypes = {
+  name: PropTypes.string,
+  className: PropTypes.string,
+  icon: PropTypes.string,
+  children: PropTypes.element
+}
+
+export default Note
