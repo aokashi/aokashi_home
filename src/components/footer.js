642deac924b8a4937ed55112b779a58ff628cd9d
Aokashi <Aokashi.Towa@Gmail.com>
2019-10-03 14:40:31 +0900

    フッターにフッターメニューを実装

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,17 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Link } from "gatsby"
 import "./footer.sass"
 
 const Footer = ({ siteTitle }) => (
   <footer className="main-footer">
     <div className="container">
+      <nav className="main-footer__nav nav">
+        <Link to="/" className="nav__item">Home</Link>
+        <Link to="/about/" className="nav__item">当サイトについて</Link>
+        <a href="http://aokashi.hatenablog.jp" className="nav__item">Blog</a>
+        <a href="https://www.wwafansq.com" className="nav__item">WWA FanSquare</a>
+      </nav>
       <div className="main-footer__copyright">
         <div>
           Copyright © 2010-{new Date().getFullYear()} {siteTitle}.
