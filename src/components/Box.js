ae51366f1a7480ff1b00ec5c34fc0a656407b517
Aokashi <Aokashi.Towa@Gmail.com>
2019-10-08 00:16:26 +0900

    Boxからリンク部分を分離し、BoxNavに独立

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,49 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import Link from './Link'
 import styles from './Box.module.sass'
 
-const Box = ({ title, className, navItems, children }) => (
+const Box = ({ title, className, children }) => (
   <div className={`column ${styles.box} ${className}`}>
     <h3 className={styles.boxTitle}>{title}</h3>
     {children}
-    <div className={styles.boxNav}>
-      {
-        navItems.map(renderNavItem)
-      }
-    </div>
   </div>
 )
-
-const renderNavItem = (navItem, navIndex) => {
-  if (navItem.isDownloadable) {
-    return (
-      <a href={navItem.link} download={navItem.link} className={styles.navItem} key={navIndex}>{navItem.name}</a>
-    )
-  }
-  return (
-    <Link href={navItem.link} className={styles.navItem} key={navIndex}>{navItem.name}</Link>
-  )
-}
 
 Box.propTypes = {
   title: PropTypes.string,
   className: PropTypes.string,
-  navItems: PropTypes.arrayOf(
-    PropTypes.shape({
-      link: PropTypes.string.isRequired,
-      name: PropTypes.string,
-      isDownloadable: PropTypes.bool,
-    })
-  ),
   children: PropTypes.node,
 }
 
 Box.defaultProps = {
   title: '',
   className: '',
-  navItems: []
 }
 
 export default Box
