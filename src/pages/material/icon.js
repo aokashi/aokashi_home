d00070cda622a64314709deb0f2e3ef48579f328
Aokashi <Aokashi.Towa@Gmail.com>
2019-10-13 23:04:02 +0900

    アイコン素材ページを実装

---
+++ src/pages/material/icon.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import Layout from '../../layouts/page-layout'
+import { StaticQuery, graphql } from 'gatsby'
+
+import PageHeader from '../../components/PageHeader'
+import LinkButton from '../../components/LinkButton'
+
+class IconPage extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <PageHeader>
+          <h1>アイコン素材</h1>
+        </PageHeader>
+        <div className="content">
+          <p>アイコンはすべて32×32のサイズに統一しています。</p>
+          <StaticQuery
+            query={graphql`
+              query IconMaterialDataQuery {
+                allIconMaterialJson {
+                  nodes {
+                    name
+                    description
+                    downloadFile
+                    files {
+                      src
+                      alt
+                      background
+                    }
+                    tags
+                  }
+                }
+              }
+            `}
+            render={data => (
+              <>
+                {
+                  data.allIconMaterialJson.nodes.map((item, index) => (
+                    <section className="section" key={index}>
+                      <h2>{item.name}</h2>
+                      <div className="block">
+                        {
+                          item.files.map((file, fileIndex) => (
+                            <img src={file.src} alt={file.alt} key={fileIndex} />
+                          ))
+                        }
+                      </div>
+                      <div className="block">
+                        <LinkButton href={item.downloadFile}>ダウンロード</LinkButton>
+                      </div>
+                      <p>{item.description}</p>
+                    </section>
+                  ))
+                }
+              </>
+            )}
+          />
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default IconPage
