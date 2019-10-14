463e727f40ba438ec56fbdcc671559178891ffa1
Aokashi <Aokashi.Towa@Gmail.com>
2019-10-14 20:29:18 +0900

    サイドバーの幅を固定化、MaterialSidebar に activeStyle を付与

--- src/components/MaterialSidebar.js
+++ src/components/MaterialSidebar.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+// TODO: page-layout のスタイルシートからクラス名が利用できないか考えておく
 const MaterialSidebar = () => (
   <>
     <ul>
       <li>
-        <Link to="/material/">素材トップ</Link>
+        <Link to="/material/" activeStyle={{ fontWeight: 'bold' }}>素材トップ</Link>
       </li>
       <li>
-        <Link to="/material/wwa/">WWA素材</Link>
+        <Link to="/material/wwa/" activeStyle={{ fontWeight: 'bold' }}>WWA素材</Link>
       </li>
       <li>
-        <Link to="/material/icon/">アイコン素材</Link>
+        <Link to="/material/icon/" activeStyle={{ fontWeight: 'bold' }}>アイコン素材</Link>
       </li>
       <li>
-        <Link to="/material/tkool_mv">ツクールMV</Link>
+        <Link to="/material/tkool_mv" activeStyle={{ fontWeight: 'bold' }}>ツクールMV</Link>
       </li>
     </ul>
   </>
