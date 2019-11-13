2e66cf414d210848d1e00dbbd4e496a8c30949e2
Aokashi <Aokashi.Towa@Gmail.com>
2019-11-14 00:02:38 +0900

    Note関係のコンポーネントのスタイルシートをBulmaに移行、utilitiesの読み込みを後に

--- src/components/Note/AboutNote.js
+++ src/components/Note/AboutNote.js
@@ -2,24 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Note from './Note'
 
-import styles from './AboutNote.module.sass'
 import AboutIcon from '../../images/note_icon-about.png'
 
 const AboutNote = ({ title, children, link, linkname }) => (
   <Note
-    className={styles.aboutNote}
+    className="is-success"
+    title={title}
     icon={AboutIcon}
   >
-    <div className={styles.content}>
-      <div className={styles.title}>{title}</div>
-      <div className={styles.description}>{children}</div>
-      {
-        link &&
-          <div className={styles.link}>
-            <a href={link}>{linkname ? linkname : title}</a>
-          </div>
-      }
-    </div>
+    {children}
+    {link &&
+      <div>
+        → <a href={link} className="link" target="_blank" rel="noopener noreferrer">{linkname ? linkname : title}</a>
+      </div>
+    }
   </Note>
 )
 
--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,21 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import styles from './Note.module.sass'
 
-const Note = ({ name, className, icon, children }) => (
-  <div className={`${styles.note} ${className}`}>
-    {
-      icon &&
-        <img className={styles.noteIcon} src={icon} alt={name}></img>
+const Note = ({ name, title, className, icon, children }) => (
+  <div className={`message block ${className}`}>
+    {title &&
+      <div className="message-header"><p>{title}</p></div>
     }
-    <div>
-      {children}
+    <div className="message-body">
+      <NoteContent icon={icon} name={name}>{children}</NoteContent>
     </div>
   </div>
 )
 
+const NoteContent = ({ icon, name, children }) => {
+  if (icon) {
+    return (
+      <div className="media">
+        <div className="media-left">
+          <img src={icon} alt={name}></img>
+        </div>
+        <div className="media-content">
+          {children}
+        </div>
+      </div>
+    )
+  }
+}
+
 Note.propTypes = {
   name: PropTypes.string,
+  title: PropTypes.string,
   className: PropTypes.string,
   icon: PropTypes.string,
   children: PropTypes.node,
