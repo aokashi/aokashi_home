40c9060a88b3e4c474e666a124d56065e08d5123
Aokashi <Aokashi.Towa@Gmail.com>
2019-11-13 23:59:50 +0900

    LinkButton を buttons で囲うように仕様変更

--- src/components/LinkButton.js
+++ src/components/LinkButton.js
@@ -4,10 +4,12 @@ import PropTypes from 'prop-types'
 import Link from './Link'
 
 const LinkButton = ({ href, children }) => (
-  <Link
-    className={`button`}
-    href={href}
-  >{children}</Link>
+  <div className="buttons is-centered block">
+    <Link
+      className="button"
+      href={href}
+    >{children}</Link>
+  </div>
 )
 
 LinkButton.propTypes = {
