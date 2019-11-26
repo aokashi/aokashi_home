8071e3c47a7381f3b7ee7df3b0e105741fef9821
Aokashi <Aokashi.Towa@Gmail.com>
2019-11-27 00:49:44 +0900

    AboutNoteからlink, linknameを削除
    AboutNoteの出力方法が自動化し、1つしかリンクが配置できない属性を使用する必要が無くなったため

--- src/components/Note/AboutNote.js
+++ src/components/Note/AboutNote.js
@@ -4,26 +4,19 @@ import Note from './Note'
 
 import AboutIcon from '../../images/note_icon-about.png'
 
-const AboutNote = ({ title, children, link, linkname }) => (
+const AboutNote = ({ title, children }) => (
   <Note
     className="is-success"
     title={title}
     icon={AboutIcon}
   >
     {children}
-    {link &&
-      <div>
-        → <a href={link} className="link" target="_blank" rel="noopener noreferrer">{linkname ? linkname : title}</a>
-      </div>
-    }
   </Note>
 )
 
 AboutNote.propTypes = {
   title: PropTypes.string,
   children: PropTypes.node,
-  link: PropTypes.string,
-  linkname: PropTypes.string,
 }
 
 export default AboutNote
