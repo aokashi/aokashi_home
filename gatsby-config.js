8127d21a950d571277752e40b4b84bfdc8f0f76c
Aokashi <Aokashi.Towa@Gmail.com>
2020-05-10 20:51:05 +0900

    gatsby-plugin-catch-links を追加、 gatsby をアップデート

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,7 +86,8 @@ module.exports = {
           families: [`Nunito`]
         }
       }
-    }
+    },
+    `gatsby-plugin-catch-links`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
