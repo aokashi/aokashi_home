eb775b226fa149790a29f21042baea9abd7a9108
Aokashi <Aokashi.Towa@Gmail.com>
2020-10-25 13:37:01 +0900

    trailingShash を削除

--- gatsby-node.js
+++ gatsby-node.js
@@ -69,7 +69,7 @@ exports.createPages = async ({ actions, graphql }) => {
     const tagName = groupItem.fieldValue
 
     createPage({
-      path: `/portfolio/tag/${tagName}/`,
+      path: `/portfolio/tag/${tagName}`,
       component: path.resolve('./src/templates/portfolio-tag.js'),
       context: {
         tag: tagName,
@@ -83,7 +83,11 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === 'MarkdownRemark') {
-    const value = createFilePath({ node, getNode })
+    const value = createFilePath({
+      node,
+      getNode,
+      trailingSlash: false,
+    })
     createNodeField({
       name: `slug`,
       node,
